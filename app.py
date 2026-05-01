098857dbd85b12c318068e81e86772da1fa89ba5
hari-charan-bhogadi <haricharan.bhogadi@gmail.com>
2026-05-02 01:39:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def add():
         image = request.files['image']
 
         if image:
+            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
             path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(path)
 
@@ -44,4 +45,7 @@ def add():
 
     return render_template('add.html')
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
